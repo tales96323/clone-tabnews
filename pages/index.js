c8457f55b0f615e4caf56f9238b446465160a314
tales96323 <47725439+tales96323@users.noreply.github.com>
2024-08-01 11:08:56 +0000

    instalação de Test, escrevi run test e alterei a mensagem geral do site

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 function Home() {
-  return <h1>Hello! Welcome to Internet! </h1>;
+  return <h1>Desenvolvendo Curriculo! </h1>;
+  <h2>Estou desenvolvemdo um curriculo em forma de site para servir de portifólio.</h2>
 }
 export default Home;
