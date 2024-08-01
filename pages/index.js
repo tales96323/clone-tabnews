bf00369da295fbb1aecb8d4f4c094c1976a7533f
tales96323 <47725439+tales96323@users.noreply.github.com>
2024-08-01 11:11:47 +0000

    Alteração de mensagem. Teve bug.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
   return <h1>Desenvolvendo Curriculo! </h1>;
-  <h2>Estou desenvolvemdo um curriculo em forma de site para servir de portifólio.</h2>
+  return <h2>Estou desenvolvemdo um curriculo em forma de site para servir de portifólio.</h2>;
 }
 export default Home;
